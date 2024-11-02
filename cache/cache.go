39067d1b487fdb4efd2f4bbe58d159efbd324c1f
AccursedGalaxy <robinbohrer7@gmail.com>
2024-11-02 21:58:53 +0100

    caching

---
+++ cache/cache.go
@@ -0,0 +1,73 @@
+package cache
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+
+	"github.com/AccursedGalaxy/streakode/scan"
+)
+
+// in-memory cache to hold repo metadata during runtime
+var Cache map[string]scan.RepoMetadata
+
+// InitCache - Initializes the in memory cache
+func InitCache() {
+	Cache = make(map[string]scan.RepoMetadata)
+}
+
+// LoadCache - loads repository metadata from a JSON cache file into memory
+func LoadCache(filePath string) error {
+	file, err := os.Open(filePath)
+	if os.IsNotExist(err) {
+		log.Printf("Cache file not found. Creating new cache.")
+		InitCache()
+		return nil
+	} else if err != nil {
+		log.Fatalf("Error opening cache file: %v", err)
+		return err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&Cache); err != nil {
+		log.Fatalf("Error decoding cache file: %v", err)
+		InitCache()
+	}
+
+	return nil
+}
+
+
+// SaveCache - saves the in-memory cache to a JSON file
+func SaveCache(filePath string) error {
+	file, err := os.Create(filePath)
+	if err != nil {
+		log.Fatalf("Error creating cache file: %v", err)
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	if err := encoder.Encode(Cache); err != nil {
+		log.Fatalf("Error encoding cache: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+
+// RefreshCache - scans directories and updates the cache with new metadata
+func RefreshCache(dirs []string, author string, cacheFilePath string) error {
+	repos, err := scan.ScanDirectories(dirs, author)
+	if err != nil {
+		log.Fatalf("Error scanning directories: %v", err)
+		return err
+	}
+	for _, repo := range repos {
+		Cache[repo.Path] = repo
+	}
+	SaveCache(cacheFilePath)
+	return nil
+}
--- main.go
+++ main.go
@@ -3,29 +3,33 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/AccursedGalaxy/streakode/cache"
 	"github.com/AccursedGalaxy/streakode/config"
 	"github.com/AccursedGalaxy/streakode/scan"
 )
 
 func main() {
 	config.LoadConfig("")
-	fmt.Printf("Config loaded: %+v\n", config.AppConfig)
+	cacheFilePath := os.Getenv("HOME") + "/.streakode_cache.json"
 
+	// Init cache and load from file
+	cache.InitCache()
+	cache.LoadCache(cacheFilePath)
+
+	// Refresh cache periodically (or on command)
 	author := config.AppConfig.Author
-	fmt.Printf("Searching for repositories with author: %s\n", author)
+	cache.RefreshCache(config.AppConfig.ScanDirectories, author, cacheFilePath)
 
+	// Scan directories and throw error if none found
 	repos, err := scan.ScanDirectories(config.AppConfig.ScanDirectories, author)
 	if err != nil {
 		log.Fatalf("Error scanning directories: %v", err)
 	}
 
+	// Log info if no active repos found
 	if len(repos) == 0 {
 		fmt.Println("No active repositories found!")
-	}
-
-	for _, repo := range repos {
-		fmt.Printf("Repo: %s, Last Commit: %s, Commit Count: %d, Dormant: %v\n", 
-			repo.Path, repo.LastCommit, repo.CommitCount, repo.Dormant)
 	}
 }
