4e16be6e43365e7a139ee4dc5eec88b28b897eba
AccursedGalaxy <robinbohrer7@gmail.com>
2024-11-11 00:15:18 +0100

    fix: removed auto update for now

--- main.go
+++ main.go
@@ -10,13 +10,11 @@ import (
 	"github.com/AccursedGalaxy/streakode/cache"
 	"github.com/AccursedGalaxy/streakode/cmd"
 	"github.com/AccursedGalaxy/streakode/config"
-    "github.com/rhysd/go-github-selfupdate/selfupdate"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-    "github.com/blang/semver"
 )
 
-var Version = "1.5.6" // This will be overwritten during build
+var Version = "dev" // This will be overwritten during build
 
 type GitHubRelease struct {
 	TagName string `json:"tag_name"`
@@ -250,67 +248,7 @@ func main() {
 		},
 	}
 
-	// Add a command to check for updates
-    updateCmd := &cobra.Command{
-        Use:   "update",
-        Short: "Check for updates and apply if available",
-        Run: func(cobraCmd *cobra.Command, args []string) {
-            updater, err := selfupdate.NewUpdater(selfupdate.Config{})
-            if err != nil {
-                fmt.Printf("Error initializing updater: %v\n", err)
-                return
-            }
-
-            repo := "AccursedGalaxy/streakode" // Your GitHub repo
-            fmt.Printf("Current version: %s\n", Version)
-
-            latest, found, err := updater.DetectLatest(repo)
-            if err != nil {
-                fmt.Printf("Error checking for updates: %v\n", err)
-                return
-            }
-            if !found {
-                fmt.Println("No latest release found.")
-                return
-            }
-            if latest == nil {
-                fmt.Println("Error: Latest release information is nil.")
-                return
-            }
-
-            // Add a check to ensure latest is not nil
-            if latest == nil {
-                fmt.Println("Error: Latest release information is nil.")
-                return
-            }
-
-            fmt.Printf("Latest version: %s\n", latest.Version)
-
-            // Parse the current version
-            currentVersion, err := semver.ParseTolerant(Version)
-            if err != nil {
-                fmt.Printf("Error parsing current version: %v\n", err)
-                return
-            }
-
-            // Compare the current version with the latest version
-            if !found || latest.Version.LTE(currentVersion) {
-                fmt.Println("You are already using the latest version.")
-                return
-            }
-
-            fmt.Printf("New version %s is available. Updating...\n", latest.Version)
-            if err := updater.UpdateTo(latest, os.Args[0]); err != nil {
-                fmt.Printf("Error updating to the latest version: %v\n", err)
-                return
-            }
-
-            fmt.Println("Update successful!")
-            fmt.Printf("Release notes: %s\n", latest.ReleaseNotes)
-        },
-    }
-
-	authorCmd := &cobra.Command{
+    authorCmd := &cobra.Command{
 		Use:   "author",
 		Short: "Show configured Git author information",
 		Run: func(cmd *cobra.Command, args []string) {
@@ -345,7 +283,6 @@ func main() {
 	rootCmd.AddCommand(cacheCmd)
 	rootCmd.AddCommand(profileCmd)
 	rootCmd.AddCommand(versionCmd)
-    rootCmd.AddCommand(updateCmd)
 	rootCmd.AddCommand(authorCmd)
 	rootCmd.Execute()
 }
