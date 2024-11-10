1fc91da6ac06a1f83b2969d212268aa37650cc27
AccursedGalaxy <robinbohrer7@gmail.com>
2024-11-11 00:39:40 +0100

    todo: new stuff todo! hurraaa!

--- main.go
+++ main.go
@@ -14,6 +14,12 @@ import (
 	"github.com/spf13/viper"
 )
 
+/*
+TODO:
+- Add automatic update functionality (manually code this cuz it's fun)
+- Add easy installation script (curl | bash)
+*/
+
 var Version = "dev" // This will be overwritten during build
 
 type GitHubRelease struct {
